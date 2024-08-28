daf3d431c8f5b667182bbed163d6d6d28540355b
Benjamin Niccum <113940036+Benjination@users.noreply.github.com>
2024-08-28 19:39:47 +0000

    Added reason for error

--- HW1_b.java
+++ HW1_b.java
@@ -2,8 +2,13 @@ public class HW1_b {
 
     public static int countPositive(int[] x) {
         int count = 0;
-        for(int i = x.length;i++)
+        for(int i = 0;i < x.length;i++)
         {
+            /*
+             * The problem with this code is inside the if loop. if(x[i]>= 0 includes the zero
+             * which should not be included in the count. By changing this to x[i] > 0, it would disclude any xeros
+             * and make the count correct
+             */
             if(x[i] >= 0)
             {
                 count++;
@@ -13,8 +18,8 @@ public class HW1_b {
     }
 
     public static void main(String[] args) {
-        int[] a = {2, 3, 5};
-        int c = findLast(a, b);
+        int[] a = {-1, 2, 0, 2};
+        int c = countPositive(a);
         
         System.out.println(c);
     }
